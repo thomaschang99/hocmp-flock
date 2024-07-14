977f6a9315957814dfeb1708ee1bbd423b241f75
thomaschang99 <thomaschang@thomastekiair.lan>
2024-07-13 23:57:42 -0700

    Committer: thomaschang99 <thomaschang@thomastekiair.lan>
    On branch main
    Changes to be committed:
    renamed:    thomas-test.js -> react-hello-world.js

--- react-hello-world.js
+++ react-hello-world.js
@@ -1,2 +1,2 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world!<br>From Thomas<br><br></h1>);
+root.render(<h1>Hello, world!<br>From Thomas<br></h1>);
